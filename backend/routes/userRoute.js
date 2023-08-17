4b79490d2254152567f0b27ce719730f3cad1dcb
eftuT <eftutesfaye357@gmail.com>
2023-08-17 09:13:42 +0300

    user Routes

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const userRouter = express.Router();
+
+const userController = require('../controller/UserController.js');
+
+// Define routes for bills resource
+userRouter.post('/', userController.create);
+userRouter.post('/login', userController.login);
+userRouter.get('/', userController.findAll);
+userRouter.get('/:id', userController.findOne);
+userRouter.put('/:id', userController.update);
+userRouter.delete('/:id', userController.delete);
+
+
+module.exports = userRouter;
